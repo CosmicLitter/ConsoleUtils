d4510441e1cead8dc35efcf7f83cfc64abcdc2e0
Brandon Holt <holtbd91@gmail.com>
2025-02-07 17:34:44 -0500

    Color escape codes

--- include/console_utils/colors.hpp
+++ include/console_utils/colors.hpp
@@ -1,44 +1,54 @@
 #pragma once
 
 #include <iostream>
-#include <string>
 
 namespace ConsoleUtils::Colors {
 
-enum class Color {
-  Default,
-  Black,
-  Red,
-  Yellow,
-  Green,
-  Blue,
-  Magenta,
-  Cyan,
-  White
+struct EscapeCode {
+  const char* code;
+  constexpr explicit EscapeCode(const char* code): code(code) {}
+
 };
 
-enum class Background {
-  Default,
-  Black,
-  Red,
-  Yellow,
-  Green,
-  Blue,
-  Magenta,
-  Cyan,
-  White
-};
+// Should be inlined because this will be duplicated across all translation
+// units that include this header.
+inline std::ostream& operator<<(std::ostream& os, const EscapeCode& c) {
+  return os << "\033[" << c.code << "m";
+}
 
-enum class Style {
-  Reset,
-  Bold,
-  Underline,
-  Blink,
-  Reverse
-};
+// Builds all EscapeCode objects at startup for static allocation
+namespace fg {
+  inline constexpr EscapeCode black("30");
+  inline constexpr EscapeCode red("31");
+  inline constexpr EscapeCode green("32");
+  inline constexpr EscapeCode yellow("33");
+  inline constexpr EscapeCode blue("34");
+  inline constexpr EscapeCode magenta("35");
+  inline constexpr EscapeCode cyan("36");
+  inline constexpr EscapeCode white("37");
+  inline constexpr EscapeCode reset("39");
+}
 
-std::string toAnsiCode(Color color);
-std::string toAnsiCode(Background color);
-std::string toAnsiCode(Style style);
+namespace bg {
+  inline constexpr EscapeCode black("40");
+  inline constexpr EscapeCode red("41");
+  inline constexpr EscapeCode green("42");
+  inline constexpr EscapeCode yellow("43");
+  inline constexpr EscapeCode blue("44");
+  inline constexpr EscapeCode magenta("45");
+  inline constexpr EscapeCode cyan("46");
+  inline constexpr EscapeCode white("47");
+  inline constexpr EscapeCode reset("49");
+}
+
+namespace style {
+  inline constexpr EscapeCode bold("1");
+  inline constexpr EscapeCode dim("2");
+  inline constexpr EscapeCode underline("4");
+  inline constexpr EscapeCode blink("5");
+  inline constexpr EscapeCode reverse("7");
+  inline constexpr EscapeCode hidden("8");
+  inline constexpr EscapeCode reset("0");
+}
 
 }
