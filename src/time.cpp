13e1193d0d30073cca64bbaecd72f61e48743607
Brandon Holt <holtbd91@gmail.com>
2025-02-09 12:49:18 -0500

    getTimestamp function cleanup

--- src/time.cpp
+++ src/time.cpp
@@ -3,17 +3,14 @@
 #include <ctime>
 #include <string>
 
+using namespace std::chrono;
 namespace ConsoleUtils::Time {
 
 std::string getCurrentTimestamp(const std::string &format) {
-  auto now = std::chrono::system_clock::now();
-  std::time_t now_time_t = std::chrono::system_clock::to_time_t(now);
+  std::time_t now = system_clock::to_time_t(system_clock::now());
 
-  std::tm local_tm;
-  localtime_r(&now_time_t, &local_tm);
-
-  char buffer[100];
-  if (std::strftime(buffer, sizeof(buffer), format.c_str(), &local_tm)) {
+  char buffer[30];
+  if (std::strftime(buffer, sizeof(buffer), format.c_str(), std::localtime(&now))) {
     return std::string(buffer);
   } else {
     return {};
